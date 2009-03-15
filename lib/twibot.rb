3658775e1de7a76ed1b1f69592bb00aced583d77
Christian Johansen <christian@cjohansen.no>
2009-03-15 18:23:34 +0100

    Upped version

--- lib/twibot.rb
+++ lib/twibot.rb
@@ -7,7 +7,7 @@ require File.join(File.dirname(__FILE__), 'hash')
 module Twibot
 
   # :stopdoc:
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
   # :startdoc:

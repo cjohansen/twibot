3f2007d8c53843920e79c7b957837a0e36af1dba
Christian Johansen <christian@cjohansen.no>
2009-06-01 00:20:37 +0200

    Updating changelog and version number for 0.1.7 release

--- lib/twibot.rb
+++ lib/twibot.rb
@@ -7,7 +7,7 @@ require File.join(File.dirname(__FILE__), 'hash')
 module Twibot
 
   # :stopdoc:
-  VERSION = '0.1.6'
+  VERSION = '0.1.7'
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
   # :startdoc:
